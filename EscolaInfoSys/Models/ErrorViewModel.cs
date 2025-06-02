c2c77eb87844dd1d875fa1df490b746a73fe7c50
JUACIRA ROSA <juacira.rosa@gmail.com>
2025-06-02 12:09:03 +0100

    Add project files.

---
+++ EscolaInfoSys/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EscolaInfoSys.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
