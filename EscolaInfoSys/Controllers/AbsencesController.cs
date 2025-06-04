854ad2c3accbd730f4772cf3ee5c892279c3d990
JUACIRA ROSA <juacira.rosa@gmail.com>
2025-06-04 19:57:58 +0100

    Add full CRUD for Absences

---
+++ EscolaInfoSys/Controllers/AbsencesController.cs
@@ -0,0 +1,128 @@
+﻿using EscolaInfoSys.Data;
+using EscolaInfoSys.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscolaInfoSys.Controllers
+{
+    [Authorize(Roles = "Administrator,StaffMember")]
+    public class AbsencesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AbsencesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var absences = _context.Absences
+                .Include(a => a.Student)
+                .Include(a => a.Subject);
+            return View(await absences.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var absence = await _context.Absences
+                .Include(a => a.Student)
+                .Include(a => a.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (absence == null) return NotFound();
+
+            return View(absence);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "FullName");
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Absence absence)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(absence);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "FullName", absence.StudentId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", absence.SubjectId);
+            return View(absence);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var absence = await _context.Absences.FindAsync(id);
+            if (absence == null) return NotFound();
+
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "FullName", absence.StudentId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", absence.SubjectId);
+            return View(absence);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Absence absence)
+        {
+            if (id != absence.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(absence);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Absences.Any(e => e.Id == id)) return NotFound();
+                    else throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "FullName", absence.StudentId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", absence.SubjectId);
+            return View(absence);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var absence = await _context.Absences
+                .Include(a => a.Student)
+                .Include(a => a.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (absence == null) return NotFound();
+
+            return View(absence);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var absence = await _context.Absences.FindAsync(id);
+            _context.Absences.Remove(absence);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- EscolaInfoSys/Models/Absence.cs
+++ EscolaInfoSys/Models/Absence.cs
@@ -1,13 +1,20 @@
-﻿namespace EscolaInfoSys.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EscolaInfoSys.Models
 {
     public class Absence
     {
         public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
+        [Required]
         public int StudentId { get; set; }
         public Student Student { get; set; }
 
+        [Required]
         public int SubjectId { get; set; }
         public Subject Subject { get; set; }
 
